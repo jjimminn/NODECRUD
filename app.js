709c727d12cca435ca7540bd9bc16a5f3792db61
jjimminn <tmddbqkqn@gmail.com>
2023-10-30 13:51:04 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// 모듈
+const express = require('express');
+const app = express();
+
+
+// 라우팅
+const home = require('./routes/home');
+
+// 앱 세팅
+app.set('views', "./views");
+app.set('view engine', 'ejs');
+
+app.use("/", home); // use는 미들 웨어 등록해줌!
+
+module.exports = app;
